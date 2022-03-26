cccda4de6b2213f989987e551b8f1b6a8f513f69
Danilo Gonçalves <gonca.danilo@gmail.com>
2022-03-26 18:35:59 -0300

    feat: create global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import 'src/styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   // eslint-disable-next-line react/jsx-props-no-spreading
