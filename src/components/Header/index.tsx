0b4d4309c4f728f24a58a13775e42bcba77c4729
Danilo Gonçalves <gonca.danilo@gmail.com>
2022-03-28 02:03:50 -0300

    feat: create dashboard page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+import { useState } from 'react';
 import { useRouter } from 'next/router';
 import { destroyCookie } from 'nookies';
 import { FaGithub } from 'react-icons/fa';
@@ -7,21 +9,37 @@ import styles from './styles.module.scss';
 
 export function Header() {
   const routes = useRouter();
+  const [username, setUsername] = useState('');
 
   function handleSignOut() {
     destroyCookie(undefined, 'github-token');
     routes.replace('/');
   }
 
+  function handleSearch(event: any) {
+    event.preventDefault();
+
+    if (username.trim()) {
+      setUsername('');
+      routes.push(`?username=${username}`);
+    }
+  }
+
   return (
     <header className={styles.header}>
       <h1>
         <FaGithub />
-        Github Profile
+        <Link href="/">Github Profile</Link>
       </h1>
 
-      <form>
-        <input type="text" placeholder="Buscar por username" name="username" />
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          placeholder="Buscar por username"
+          name="username"
+          onChange={(event) => setUsername(event.target.value)}
+          value={username}
+        />
       </form>
 
       <button
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,17 +1,51 @@
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 import { parseCookies } from 'nookies';
+import { FaTwitter } from 'react-icons/fa';
+import { FiUsers, FiLink, FiMapPin, FiMail } from 'react-icons/fi';
+import { BsDot } from 'react-icons/bs';
+import { BiBuildings } from 'react-icons/bi';
 
 import { Header } from 'src/components/Header';
 import { api } from 'src/service/github';
 import { User } from 'src/types/user';
 
-// import styles from 'src/styles/dashboard.module.scss';
+import styles from 'src/styles/dashboard.module.scss';
 
 interface DashboardProps {
   user: User;
+  token: string;
 }
 
-export default function Dashboard({ user }: DashboardProps) {
+interface Repository {
+  id: number;
+  name: string;
+}
+
+export default function Dashboard({ user, token }: DashboardProps) {
+  const [repositories, setRepositories] = useState<Repository[]>(
+    [] as Repository[],
+  );
+
+  async function loadRepositories() {
+    try {
+      const { data: repositoriesData } = await api.get(
+        `${user.repos_url}?page=1&per_page=5`,
+        {
+          headers: { Authorization: `token ${token}` },
+        },
+      );
+
+      setRepositories(repositoriesData);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    loadRepositories();
+  }, [user]);
+
   return (
     <>
       <Head>
@@ -19,11 +53,77 @@ export default function Dashboard({ user }: DashboardProps) {
       </Head>
 
       <Header />
+
+      <main className={styles.container}>
+        <section className={styles.hero}>
+          <img src={user.avatar_url} alt={user.name} />
+
+          <div className={styles.userInfoGroup}>
+            <h1>{user.name}</h1>
+            <h3>{user.login}</h3>
+            <p>{user.bio}</p>
+          </div>
+
+          <div className={styles.followersGroup}>
+            <span>
+              <FiUsers />
+              <span>{user.followers}</span> seguidores
+            </span>
+            <span>
+              <BsDot />
+              <span>{user.following}</span> seguindo
+            </span>
+          </div>
+
+          <div className={styles.socialGroup}>
+            {user.company && (
+              <span>
+                <BiBuildings />
+                {user.company}
+              </span>
+            )}
+            {user.location && (
+              <span>
+                <FiMapPin />
+                {user.location}
+              </span>
+            )}
+            {user.email && (
+              <span>
+                <FiMail />
+                {user.email}
+              </span>
+            )}
+            {user.blog && (
+              <span>
+                <FiLink />
+                {user.blog}
+              </span>
+            )}
+            {user.twitter_username && (
+              <span>
+                <FaTwitter />
+                {user.twitter_username}
+              </span>
+            )}
+          </div>
+        </section>
+        <div className={styles.divider} />
+        <section className={styles.content}>
+          <h2>Repos</h2>
+          <ul>
+            {repositories.map((repository) => (
+              <li key={repository.id}>{repository.name}</li>
+            ))}
+          </ul>
+        </section>
+      </main>
     </>
   );
 }
 export async function getServerSideProps(ctx: any) {
   const { 'github-token': token } = parseCookies(ctx);
+  const { username } = ctx.query;
 
   if (!token) {
     return {
@@ -34,11 +134,14 @@ export async function getServerSideProps(ctx: any) {
     };
   }
 
-  const { data: user } = await api.get('/user', {
-    headers: { Authorization: `token ${token}` },
-  });
+  const { data: user } = await api.get(
+    username ? `/users/${username}` : '/user',
+    {
+      headers: { Authorization: `token ${token}` },
+    },
+  );
 
   return {
-    props: { user },
+    props: { user, token },
   };
 }
