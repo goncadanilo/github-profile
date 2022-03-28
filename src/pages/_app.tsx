a9d16819c5b29189393ff7141b6b328e79e079fa
Danilo Gonçalves <gonca.danilo@gmail.com>
2022-03-28 20:36:10 -0300

    feat: configure react query provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { AppProps } from 'next/app';
-
+import { QueryClient, QueryClientProvider } from 'react-query';
 import 'src/styles/global.scss';
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
